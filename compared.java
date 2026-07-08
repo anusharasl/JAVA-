e6b5c53da5fe33adc4d402026a7d50eecb03aa27
ANUSHARA S.L <anushara429@gmail.com>
2026-07-09 00:01:49 +0530

    day2/

---
+++ compared.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class compared {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String meghana = sc.nextLine();
+        if(meghana.equals("dead"))
+        {
+            System.out.print("suriya meets ramya");
+        }
+        else
+        {
+            System.out.print("suriya weds meghana");
+        }
+    }
+}
+
