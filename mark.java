f479ce0f7d2b770b7ff091b12f047c8a942f5ae5
ANUSHARA S.L <anushara429@gmail.com>
2026-07-11 00:25:58 +0530

    day3/

---
+++ mark.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class mark{
+    public static void main(String args[])
+    {
+     int mark=35;
+     if(mark>=35)
+     {
+        System.out.println("PASS");
+     }
+     else
+     {
+        System.out.println("FAIL");
+     }
+    }
+}
