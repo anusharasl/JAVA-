360e2b4b4dd98b18dac08e44fb754bff6ec3588b
ANUSHARA S.L <anushara429@gmail.com>
2026-07-12 20:32:36 +0530

    day4/

---
+++ divide.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class divide{
+    public static void main(String args[])
+    {
+     int num = 10;
+     if(num%3 ==0 && num%5==0)
+     {
+        System.out.print("divisible");
+     } 
+     else
+    {
+        System.out.print("no");
+    }
+    }
+
+}
