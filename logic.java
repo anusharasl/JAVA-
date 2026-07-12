df5b844ed4031c30873b3cb072636c5916e68815
ANUSHARA S.L <anushara429@gmail.com>
2026-07-12 20:53:48 +0530

    day4/

---
+++ logic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class logic{
+    public static void main(String args[])
+    {
+    boolean cricket = true;
+    boolean football = false;
+    if(cricket && football)
+    {
+        System.out.print("play");
+    }   
+    else{
+        System.out.print("not play");
+    }
+    }
+}
