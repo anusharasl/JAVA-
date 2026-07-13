36767a2b14a9e65456d4929aea914c5edb0bbaaa
ANUSHARA S.L <anushara429@gmail.com>
2026-07-13 20:24:18 +0530

    day5/

---
+++ ternary.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ternary{
+    public static void main(String args[])
+    {
+        boolean rain = false;
+        String result = rain?"take an umberlla":"Enjoy the sunlight";
+        System.out.print(result);
+    }
+}
