afd4846a2ee2bf22a02b4924478915ff0cbe185b
ANUSHARA S.L <anushara429@gmail.com>
2026-07-15 23:07:47 +0530

    day6/

---
+++ act.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class act{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a = sc.nextInt();
+        System.out.print("Enter b:");
+        int b = sc.nextInt();
+        int count = 0;
+        for(int i=a;i<=b;i=i+1)
+        {
+         if(i%2==0)
+         {
+            count=count+1;
+         }
+        }
+        System.out.print(count);  
+    
+    }
+}
