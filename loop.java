f77ebc94ec85dc60565ec432d637f7f05663edbe
ANUSHARA S.L <anushara429@gmail.com>
2026-07-15 22:38:43 +0530

    day6/

---
+++ loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class chal{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a = sc.nextInt();
+        System.out.print("Enter b:");
+        int b = sc.nextInt();
+        for(int i=a;i<=b;i=i+1)
+        {
+            if(i%2==0)
+            {
+            System.out.println(i);
+            }
+        }
+    }
+}
