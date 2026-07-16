043e6e3e258fd63a75d6264a267ee6cf6c4cba0b
ANUSHARA S.L <anushara429@gmail.com>
2026-07-16 19:36:44 +0530

    day7/

---
+++ cls.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class cls{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=100;i=i+1)
+        {
+            if(i%3 == 0  && i%5 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
