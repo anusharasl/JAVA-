f17e3946b1c08cb695207ed59c94ba4c191d7610
ANUSHARA S.L <anushara429@gmail.com>
2026-07-16 20:24:43 +0530

    day7/

---
+++ bcd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class bcd{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int[] marks = new int[5];
+        for(int i=0;i<=4;i++)
+        {
+            marks[i] = sc.nextInt();
+        }
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+    }
+}
