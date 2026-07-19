58066a596492de6d4bc114a736e6e669aa209284
ANUSHARA S.L <anushara429@gmail.com>
2026-07-19 12:38:20 +0530

    day8/

---
+++ an.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class an{
+    public static void main(String args[])
+    {
+        for(int teacher = 1;teacher <= 3;teacher = teacher+1)
+        {
+        for(int i = 1;i <=5; i = i + 1)
+        {
+            System.out.println("john");
+        }
+       }
+    }
+}
