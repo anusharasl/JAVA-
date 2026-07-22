a50d0fd832e3d7e6da8341349bd6478b01d0fa99
ANUSHARA S.L <anushara429@gmail.com>
2026-07-22 21:24:51 +0530

    d/

---
+++ exe.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class exe{
+    public static void main(String args[]){
+        int count =0;
+        do{
+            System.out.println("john");
+            count=count+1;
+        }
+        while(count<=1);
+    }
+}
