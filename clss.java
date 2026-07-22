c50470c7f7c2f78c0a07e1066d66ea3ad34e05bb
ANUSHARA S.L <anushara429@gmail.com>
2026-07-22 22:03:03 +0530

    d/

---
+++ clss.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class clss{
+    int coffee = 10;
+    int tea = 20;
+    public static void main(String args[]){
+        clss server1 = new clss();
+        System.out.print(server1.coffee);
+        clss server2 = new clss();
+        System.out.print(server2.tea);
+    }
+}
